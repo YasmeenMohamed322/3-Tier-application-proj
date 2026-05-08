f52fe6b6704ce76e5a9978cd204072851cb31179
Tasneem Adel <tasneem.adel8@gmail.com>
2026-05-08 17:55:57 +0300

    add App

---
+++ App/backend/dbsql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS patients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    condition TEXT NOT NULL,
+    status VARCHAR(20) CHECK (status IN ('stable', 'not stable')) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
