edb5746ff9ac8c0712bb0b11960c93b57170959d
Roumaysaa <samyroumaysaa@gmail.com>
2026-05-09 21:43:52 +0300

    Update Vault provider
    Reintroduced the Vault provider configuration and updated AWS provider to use credentials from Vault.

--- provider.tf
+++ provider.tf
@@ -4,23 +4,25 @@ terraform {
         source = "hashicorp/aws" 
         version = "5.36.0" 
     }
-    # vault = {
-    #   source  = "hashicorp/vault"
-    #   version = "~> 3.0"
-    # }
-    
+    vault = {
+       source  = "hashicorp/vault"
+       version = "~> 3.0"
+    } 
   } 
 } 
 
-#AWS Provider
-provider "aws" { 
-    region = "us-east-1"
-    access_key = var.AWS_ACCESS_KEY_ID  
-    secret_key = var.AWS_SECRET_ACCESS_KEY
+#Vault Provider
+provider "vault" { }
+
+data "vault_kv_secret_v2" "aws_creds" {
+  mount = "secret"
+  name  = "aws"
 }
 
-# # Vault Provider
-# provider "vault" {
-#   address = var.vault_addr
-#   token   = var.vault_token
-# }
+#AWS Provider
+provider "aws" {
+  region = "us-east-1"
+
+  access_key = data.vault_kv_secret_v2.aws_creds.data["access_key"]
+  secret_key = data.vault_kv_secret_v2.aws_creds.data["secret_key"]
+}
